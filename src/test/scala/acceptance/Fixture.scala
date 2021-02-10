15c777aedc817bd424dc0dc96916332420ce5204
Jean-Luc Canela <jlcanelam@gmail.com>
2021-02-10 14:48:16 +0100

    add gen-hubconfig command (#14)

--- src/test/scala/acceptance/Fixture.scala
+++ src/test/scala/acceptance/Fixture.scala
@@ -10,5 +10,7 @@ trait Fixture {
     Array("gen-model", "-i", s"$inputFolder/test.zip", "-o", s"$outputFolder/model.json")
   val cmdExtractFilesSuccessArgs =
     Array("extract-files", "-i", s"$inputFolder/test.zip", "-o", s"$outputFolder/extract")
+ val cmdGenHubconfigSuccessArgs =
+   Array("gen-hubconfig", "-i", s"$inputFolder/model.json", "-o", s"$outputFolder/hubconfig.json")
 
 }
